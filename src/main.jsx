0345e28977579f3fb3df79a6fe37d60602623e55
RomaanDeRose <romandr37@gmail.com>
2025-05-11 21:51:52 -0300

    Renombrar el componente de productos a AllProducts en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
-import Products from "./pages/Products";
+import AllProducts from "./pages/AllProducts";
 import ProductDetail from "./pages/ProductDetail";
 import "./index.css";
 
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products">
-          <Route index element={<Products />} />
+          <Route index element={<AllProducts />} />
           <Route path=":id" element={<ProductDetail />} />
         </Route>
       </Routes>
