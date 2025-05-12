1790b963a86918fca508aa69e0f0cf1e52edbba2
RomaanDeRose <romandr37@gmail.com>
2025-05-11 23:31:47 -0300

    Estilos page Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import logo from '../assets/logo.png';
 
 function Navbar() {
     return (
-      <header className="fixed top-0 left-0 z-10 w-full py-2.5 bg-white border-b-1 shadow-md shadow-gray-200/20 border-gray-100/50">
-        <nav className="max-w-4xl mx-auto flex justify-between items-center px-4 md:px-2">
+      <header className="fixed top-0 left-0 z-10 w-full bg-white border-b-1 shadow-md shadow-gray-200/20 border-gray-100/50">
+        <nav className="max-w-4xl mx-auto flex justify-between items-center py-3 px-4 md:px-2">
           <NavLink to="/">
             <img src={logo} alt="Logo Magic Store" className="size-9" />
           </NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,25 @@
+import { Link } from "react-router";
 import Navbar from "../components/Navbar";
 
 function Home() {
   return (
-    <div className="w-full">
+    <>
       <Navbar />
-      <h1 className="text-blue-500 text-5xl font-bold text-center text-balance my-4">
-        Welcome to the Magic Store
-      </h1>
-      <p>This is the home page of our application.</p>
-    </div>
+      <main className="max-w-4xl h-screen flex flex-col justify-center items-center gap-3 mx-auto px-4 md:px-2">
+        <div className="w-80 sm:w-96">
+          <h1 className="text-blue-500 text-5xl sm:text-6xl font-extrabold text-center text-balance mb-4">
+            Welcome to the Magic Store!
+          </h1>
+          <p className="text-gray-500 text-sm sm:text-[15px] text-center">
+            Discover a wide range of products at unbeatable prices. Shop now and
+            enjoy exclusive discounts!
+          </p>
+        </div>
+        <Link to="/products" className="flex gap-1 bg-blue-500 text-white text-lg p-3 rounded-lg uppercase font-medium mt-4 transition-all hover:bg-blue-600 hover:shadow-lg hover:shadow-blue-500/30">
+          view products
+        </Link>
+      </main>
+    </>
   );
 }
 
