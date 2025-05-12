49c2046895994c4b88cb7519a7e80c17a3526b86
RomaanDeRose <romandr37@gmail.com>
2025-05-11 21:52:00 -0300

    Remove unnecessary blank lines in getProductById.js and getProducts.js

--- src/api/getProductById.js
+++ src/api/getProductById.js
@@ -2,7 +2,6 @@ const getProductById = async (id) => {
   try {
     const response = await fetch(`https://dummyjson.com/products/${id}`);
     const data = await response.json();
-
     return data;
   } catch (error) {
     console.error("Error fetching products:", error);
